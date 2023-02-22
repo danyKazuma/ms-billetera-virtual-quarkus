9c68610401c72fa098af04ce53bf761824acfdcc
Daniel Emilio Chavez Pimentel <dchavezp@emeal.nttdata.com>
2023-02-22 12:43:15 -0500

    first commit

---
+++ src/test/java/org/nttdata/bootcampIT.java
@@ -0,0 +1,8 @@
+package org.nttdata;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class bootcampIT extends bootcampTest {
+    // Execute the same tests but in packaged mode.
+}
